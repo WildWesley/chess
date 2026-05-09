b3c8bfdca6ac23c79f9e67bc4aae6c6a3280196d
WildWesley <hillspirit2021@gmail.com>
2026-05-09 13:03:51 -0600

    Changed testing format

---
+++ server/src/test/java/service/ChessServiceTest.java
@@ -0,0 +1,36 @@
+package service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ChessServiceTest {
+
+    @Test
+    void register() {
+    }
+
+    @Test
+    void clearApplication() {
+    }
+
+    @Test
+    void login() {
+    }
+
+    @Test
+    void logout() {
+    }
+
+    @Test
+    void listGames() {
+    }
+
+    @Test
+    void createGame() {
+    }
+
+    @Test
+    void joinGame() {
+    }
+}
