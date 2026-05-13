8155d942ac3c9f8be70fd7b671bda34efc895238
WildWesley <hillspirit2021@gmail.com>
2026-05-12 20:16:39 -0600

    Positive User tests

---
+++ server/src/test/java/dataaccess/DataAccessTests.java
@@ -0,0 +1,192 @@
+package dataaccess;
+
+import chess.ChessGame;
+import dataaccess.AuthDataAccess;
+import dataaccess.DataAccessException;
+import dataaccess.GameDataAccess;
+import dataaccess.UserDataAccess;
+import model.*;
+import org.eclipse.jetty.server.Authentication;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import service.ChessService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DataAccessTests {
+    ChessService testService;
+    AuthDataAccess testAuthDataAccess;
+    UserDataAccess testUserDataAccess;
+    GameDataAccess testGameDataAccess;
+    UserData testUserData = new UserData("bob", "bob", "bob");
+    RegisterRequest testRequest = new RegisterRequest("bob", "bob", "bob");
+    LoginRequest testLoginRequest = new LoginRequest("bob", "bob");
+
+    @BeforeEach
+    void createService() {
+        testService = new ChessService();
+
+        testAuthDataAccess = testService.getAuthDataAccess();
+
+        testUserDataAccess = testService.getUserDataAccess();
+
+        testGameDataAccess = testService.getGameDataAccess();
+    }
+
+    @Test
+    void createUserPos() throws DataAccessException {
+        testUserDataAccess.createUser(testUserData);
+    }
+
+    @Test
+    void getUserPos() throws DataAccessException {
+        testUserDataAccess.createUser(testUserData);
+        UserData accessUserData = testUserDataAccess.getUser(testUserData.username());
+        Assertions.assertEquals(accessUserData, testUserData);
+    }
+
+    @Test
+    void clearUserPos() throws DataAccessException {
+        testUserDataAccess.createUser(testUserData);
+        testUserDataAccess.clear();
+        Assertions.assertNull(testUserDataAccess.getUser(testUserData.username()));
+    }
+
+    @Test
+    void clearApplicationPositive() throws DataAccessException {
+        AuthData testAuthData = new AuthData("authToken", "bob");
+        testAuthDataAccess.createAuth(testAuthData);
+
+        UserData testUserData = new UserData("bob", "bob", "bob");
+        testUserDataAccess.createUser(testUserData);
+
+        testGameDataAccess.createGame("bob");
+
+        testService.clearApplication();
+
+        Assertions.assertNull(testAuthDataAccess.getAuth("authToken"));
+        Assertions.assertNull(testUserDataAccess.getUser("bob"));
+        Assertions.assertNull(testGameDataAccess.getGame(1));
+    }
+
+    @Test
+    void loginPositive() throws DataAccessException {
+        testService.register(testRequest);
+
+        AuthData userAuthData = testService.login(testLoginRequest);
+
+        Assertions.assertEquals("bob", userAuthData.username());
+        Assertions.assertNotNull(userAuthData.authToken());
+
+        UserData accessUserData = testUserDataAccess.getUser("bob");
+        UserData testUserData = new UserData("bob", "bob", "bob");
+        Assertions.assertEquals(accessUserData, testUserData);
+
+        // Test auth
+        AuthData accessAuthData = testAuthDataAccess.getAuth(userAuthData.authToken());
+        Assertions.assertEquals("bob", accessAuthData.username());
+    }
+
+    @Test
+    void logoutPositive() throws DataAccessException {
+        testService.register(testRequest);
+        AuthData testAuthData = testService.login(testLoginRequest);
+        testService.logout(new LogoutRequest(testAuthData.authToken()));
+
+        Assertions.assertNull(testAuthDataAccess.getAuth(testAuthData.authToken()));
+    }
+
+    @Test
+    void listGamesPositive() throws DataAccessException {
+        AuthData testAuthData = testService.register(testRequest);
+        testService.createGame(new CreateGameRequest("bob", testAuthData.authToken()));
+        ListGamesResponse listResponse = testService.listGames(new ListGamesRequest(testAuthData.authToken()));
+        GameData testGame = new GameData(1,
+                null,
+                null,
+                "bob",
+                testGameDataAccess.getGame(1).game());
+        // "List.of()" lets me set the ArrayList to that initial value
+        Assertions.assertEquals(new ListGamesResponse(new ArrayList<GameData>(List.of(testGame))), listResponse);
+    }
+
+    @Test
+    void createGamePositive() throws DataAccessException {
+        AuthData testAuthData = testService.register(testRequest);
+        CreateGameResponse testGameResponse =
+                testService.createGame(new CreateGameRequest("bob", testAuthData.authToken()));
+        Assertions.assertEquals(1, testGameResponse.gameID());
+
+        GameData accessGameData = testGameDataAccess.getGame(1);
+        Assertions.assertNotNull(accessGameData);
+    }
+
+    @Test
+    void joinGamePositive() throws DataAccessException {
+        AuthData testAuthData = testService.register(testRequest);
+        CreateGameResponse testGameResponse =
+                testService.createGame(new CreateGameRequest("bob", testAuthData.authToken()));
+        JoinGameRequest testJoinRequest = new JoinGameRequest("WHITE", 1, testAuthData.authToken());
+        testService.joinGame(testJoinRequest);
+
+        // Assess that game player WHITE is bob
+        Assertions.assertEquals("bob", testGameDataAccess.getGame(1).whiteUsername());
+    }
+
+    @Test
+    void registerFail() {
+        Assertions.assertThrows(DataAccessException.class, () -> {
+            RegisterRequest testRequest = new RegisterRequest("bob", null, "bob");
+            AuthData testAuthData = testService.register(testRequest);
+        });
+    }
+
+    @Test
+    void loginFail() {
+        Assertions.assertThrows(DataAccessException.class, () -> {
+            testService.register(testRequest);
+            AuthData userAuthData = testService.login(new LoginRequest("joe", "bob"));
+        });
+    }
+
+    @Test
+    void logoutFail() throws DataAccessException {
+        Assertions.assertThrows(DataAccessException.class, () -> {
+            testService.register(testRequest);
+            testService.login(testLoginRequest);
+            testService.logout(new LogoutRequest("authToken"));
+        });
+    }
+
+    @Test
+    void listGamesFail() throws DataAccessException {
+        Assertions.assertThrows(DataAccessException.class, () -> {
+            AuthData testAuthData = testService.register(testRequest);
+            testService.createGame(new CreateGameRequest("bob", testAuthData.authToken()));
+            ListGamesResponse listResponse = testService.listGames(new ListGamesRequest("authToken"));
+        });
+    }
+
+    @Test
+    void createGameFail() {
+        Assertions.assertThrows(DataAccessException.class, () -> {
+            AuthData testAuthData = testService.register(testRequest);
+            CreateGameResponse testGameResponse =
+                    testService.createGame(new CreateGameRequest("bob", "authToken"));
+        });
+    }
+
+    @Test
+    void joinGameFail() {
+        Assertions.assertThrows(DataAccessException.class, () -> {
+            AuthData testAuthData = testService.register(testRequest);
+            testService.createGame(new CreateGameRequest("bob", testAuthData.authToken()));
+            JoinGameRequest testJoinRequest = new JoinGameRequest("WHITE", 3, testAuthData.authToken());
+            testService.joinGame(testJoinRequest);
+        });
+    }
+}
